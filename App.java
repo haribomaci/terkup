922837f1d9da2ffd1d79296992e8b0848ff3d43e
toth julia <t.julia982@gmai.com>
2022-02-16 09:58:34 +0100

    Kezdes

---
+++ App.java
@@ -0,0 +1,18 @@
+/*
+* File: App.java
+* Author: Tóth Júlia
+* Copyright: 2022, Tóth Júlia
+* Group: Szoft 1/n
+* Date: 2022-02-16
+* Github: https://github.com/haribomaci/
+* Licenc: GNU GPL
+*/
+
+import java
+public class App {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
